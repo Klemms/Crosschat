5f195384be5872815fb03ef1da2bdb75f6e2108b
Clément Ababou <clement.ababou@outlook.com>
2021-12-11 09:45:45 +0100

    Base

---
+++ src/main/java/ovh/klemms/crosschat/Crosschat.java
@@ -0,0 +1,16 @@
+package ovh.klemms.crosschat;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class Crosschat extends Plugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
